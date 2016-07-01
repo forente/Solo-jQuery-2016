c86dbea4ba3be0a43c3b28a79f4673cef916f2b1
forente <zerofox16@gmail.com>
2016-07-01 15:31:36 -0500

    Added suqares and Increment

--- square.js
+++ square.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
+  var totalRed = 0;
+  var totalYellow =0;
+  var totalGreen = 0;
+  var totalBlue = 0;
 
-  console.log("hello");
+  function updateTotal(color){
+
+    switch (color) {
+      case 'red':
+      totalRed++;
+      $('#red').text('Total red: '+totalRed);
+        break;
+      case 'yellow':
+      totalYellow++;
+      $('#yellow').text('Total yellow: '+totalYellow);
+        break;
+      case 'green':
+      totalGreen++;
+      $('#green').text('Total green: '+totalGreen);
+        break;
+      case 'blue':
+      totalBlue++;
+      $('#blue').text('Total blue: '+totalBlue);
+        break;
+      default:
+    }
+  }
+
+  $('button').on('click', function(){
+      updateTotal($(this).data().color);
+      $('.container').append('<div class="color-cube '+ $(this).data().color+ ' "></div>');
+      
+  });
 });
